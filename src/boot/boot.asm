bdaa35654c32e05b9b9f117a1c2af52b7c9e099e
Naim <naim.baijid@gmail.com>
2026-01-01 17:32:43 +0600

    enable paging

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -12,7 +12,16 @@ dd MBFLAGS
 dd CHECKSUM
 
 
+
 section .bss
+
+align 4096
+page_directory:
+resb 4096 ; 1024 entries
+
+page_table:
+resb 4096 ; 1024 entries
+
 align 16
 stack_end:
 resb 16384 ; 16 kb reserved for stack
@@ -24,6 +33,40 @@ global _start
 extern kmain
 _start:
 mov esp, stack_top
+
+; set page directory location in cr3
+mov eax, page_directory
+mov cr3, eax
+
+
+; construct page table entries
+mov ecx, 0
+mov eax, 0
+.pt_loop:
+	mov ebx, eax
+	or ebx, 1
+	or ebx, 2
+	mov [page_table + ecx*4], ebx
+
+	add eax, 4096
+	add ecx, 1
+	cmp ecx, 1024
+	jne .pt_loop
+
+
+; map page table to pdt
+mov eax, page_table
+or eax, 1
+or eax, 2
+mov [page_directory], eax
+
+
+
+; enable paging
+mov ebx, cr0
+or ebx, 0x80000000
+mov cr0, ebx
+
 
 cli ; disable interrupt
 call kmain ; call c kernel code
